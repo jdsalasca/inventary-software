2310591d81546d23b989bb7d4510c7007a2f8023
juan salas <jdsalasc@unal.edu.co>
2023-08-02 22:23:32 -0500

    VII

--- src/main/java/com/sena/inventarioback/config/WebConfig.java
+++ src/main/java/com/sena/inventarioback/config/WebConfig.java
@@ -4,19 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-               // .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://localhost:12000")
-        .allowedOrigins("*")        
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization");
-                //.allowCredentials(true);
-    }
-    
-    
-    
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//               // .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://localhost:12000")
+//        .allowedOrigins("*")        
+//        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization");
+//                //.allowCredentials(true);
+//    }
+//    
+//    
+//    
+//}
