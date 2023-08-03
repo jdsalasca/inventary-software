bbb4010b72fa3d5f4b5070e72dae8f700c4bdfa8
juan salas <jdsalasc@unal.edu.co>
2023-08-02 21:59:59 -0500

    feat(cors):allowed IIII

--- src/main/java/com/sena/inventarioback/config/WebConfig.java
+++ src/main/java/com/sena/inventarioback/config/WebConfig.java
@@ -4,18 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Replace with your Vue app URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true);
-    }
-    
-    
-    
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*") // Replace with your Vue app URL
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+//                .allowCredentials(true);
+//    }
+//    
+//    
+//    
+//}
